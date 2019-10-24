f0c2f77b2ebf1c3b324601f7db62cd5c9dc2062d
bodnar17 <vasyabodnar19@gmail.com>
2019-10-24 10:23:33 +0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+#include<math.h>
+
+int main()
+{
+using namespace std;
+int x=1;
+float a;
+a=pow(((abs(x-1)+pow(exp(x),-x)-12.34)/(log10(sqrt(abs(x)))-cos(x*x*x))),-0.4);
+cout<< a;
+_getch();
+return 0;
+}
+
